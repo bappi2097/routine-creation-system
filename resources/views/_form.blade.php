30a9cb69c4e216325e2365e2b662fbb5c5a36d4d
Biddut Bhowmik <54990169+bhowmik23@users.noreply.github.com>
2020-10-05 13:26:01 +0600

    sidebar create

--- resources/views/_form.blade.php
+++ resources/views/_form.blade.php
@@ -1,117 +1,107 @@
-<!DOCTYPE html>
-<html lang="en">
+@include('_sidebar')
+    <div class="w-1/2 m-auto my-12 bg-white p-10 rounded-md page-content">
+        <div class="container">
+            <form method="POST" action="{{route('store')}}" enctype="multipart/form-data">
+                @csrf
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="routine">
+                        Import Routine File
+                    </label>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Form</title>
-    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-</head>
-
-<body class="bg-gray-200 align-baseline">
-    <div class="w-1/2 m-auto my-12 bg-white p-10 rounded-md">
-        <form method="POST" action="{{route('store')}}" enctype="multipart/form-data">
-            @csrf
-            <div class="mb-6">
-                <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="routine">
-                    Import Routine File
-                </label>
-
-                <input class="border border-gray-400 p-2 w-full" type="file" name="routine" id="routine"
-                    placeholder="Enter Excel" required accept=".xlsx, .xls, .csv">
-                @error('routine')
-                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                @enderror
-            </div>
-            <div class="mb-6">
-                <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
-                    Submit
-                </button>
-            </div>
-        </form>
+                    <input class="border border-gray-400 p-2 w-full" type="file" name="routine" id="routine"
+                        placeholder="Enter Excel" required accept=".xlsx, .xls, .csv">
+                    @error('routine')
+                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-6">
+                    <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
-    <div class="w-full m-auto my-12 mt-lg-auto p-40 rounded-md">
-    <table class="table-auto border border-gray-400 bg-white mx-4">
-        <thead>
-            <tr>
-                <th class="border border-gray-300 px-4 py-2">SL.</th>
-                <th class="border border-gray-300 px-4 py-2">Semester</th>
-                <th class="border border-gray-300 px-4 py-2">Section</th>
-                <th class="border border-gray-300 px-4 py-2">Students</th>
-                <th class="border border-gray-300 px-4 py-2">CRE</th>
-                <th class="border border-gray-300 px-4 py-2">Course Code</th>
-                <th class="border border-gray-300 px-4 py-2">Course Name</th>
-                <th class="border border-gray-300 px-4 py-2">A</th>
-                <th class="border border-gray-300 px-4 py-2">B</th>
-                <th class="border border-gray-300 px-4 py-2">C</th>
-                <th class="border border-gray-300 px-4 py-2">D</th>
-                <th class="border border-gray-300 px-4 py-2">E</th>
-                <th class="border border-gray-300 px-4 py-2">F</th>
-
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($routines as $index=>$routine)
-                @foreach($routine->courses as $course_index=>$course)
+    <div class="rounded-md page-content">
+        <div class="container">
+            <table class="table-auto border border-gray-400 bg-white mx-4">
+                <thead>
                     <tr>
-                        @if ($course_index==0)
+                        <th class="border border-gray-300 px-4 py-2">SL.</th>
+                        <th class="border border-gray-300 px-4 py-2">Semester</th>
+                        <th class="border border-gray-300 px-4 py-2">Section</th>
+                        <th class="border border-gray-300 px-4 py-2">Students</th>
+                        <th class="border border-gray-300 px-4 py-2">CRE</th>
+                        <th class="border border-gray-300 px-4 py-2">Course Code</th>
+                        <th class="border border-gray-300 px-4 py-2">Course Name</th>
+                        <th class="border border-gray-300 px-4 py-2">A</th>
+                        <th class="border border-gray-300 px-4 py-2">B</th>
+                        <th class="border border-gray-300 px-4 py-2">C</th>
+                        <th class="border border-gray-300 px-4 py-2">D</th>
+                        <th class="border border-gray-300 px-4 py-2">E</th>
+                        <th class="border border-gray-300 px-4 py-2">F</th>
 
-                            <td class="border px-4 py-2" rowspan="{{count($routine->courses)}}">
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($routines as $index=>$routine)
+                        @foreach($routine->courses as $course_index=>$course)
+                            <tr>
+                                @if ($course_index==0)
 
-                                {{ $index+1}}
+                                    <td class="border px-4 py-2" rowspan="{{count($routine->courses)}}">
 
-                            </td>
+                                        {{ $index+1}}
 
-                            <td class="border px-4 py-2" rowspan="{{count($routine->courses)}}">
+                                    </td>
 
-                                {{ $routine->semester}}
+                                    <td class="border px-4 py-2" rowspan="{{count($routine->courses)}}">
 
-                            </td>
+                                        {{ $routine->semester}}
 
-                            <td class="border px-4 py-2" rowspan="{{count($routine->courses)}}">
+                                    </td>
 
-                                @foreach ($routine->sections as $section)
+                                    <td class="border px-4 py-2" rowspan="{{count($routine->courses)}}">
 
-                                    {{$section}} {{$loop->last ? '' : ','}}
+                                        @foreach ($routine->sections as $section)
+
+                                            {{$section}} {{$loop->last ? '' : ','}}
+
+                                        @endforeach
+
+                                    </td>
+
+                                    <td class="border px-4 py-2" rowspan="{{count($routine->courses)}}">
+
+                                        {{ $routine->number_of_student}}
+
+                                    </td>
+
+                                @endif
+
+                                <td class="border px-4 py-2">
+                                    {{$course['credit']}}
+                                </td>
+
+                                <td class="border px-4 py-2">
+                                    {{$course['code']}}
+                                </td>
+
+                                <td class="border px-4 py-2">
+                                    {{$course['name']}}
+                                </td>
+
+                                @foreach ($course['teachers'] as $teacher)
+
+                                    <td class="border px-4 py-2">
+                                        {{$teacher["initial"]}}
+                                    </td>
 
                                 @endforeach
-
-                            </td>
-
-                            <td class="border px-4 py-2" rowspan="{{count($routine->courses)}}">
-
-                                {{ $routine->number_of_student}}
-
-                            </td>
-
-                        @endif
-
-                        <td class="border px-4 py-2">
-                            {{$course['credit']}}
-                        </td>
-
-                        <td class="border px-4 py-2">
-                            {{$course['code']}}
-                        </td>
-
-                        <td class="border px-4 py-2">
-                            {{$course['name']}}
-                        </td>
-
-                        @foreach ($course['teachers'] as $teacher)
-
-                            <td class="border px-4 py-2">
-                                {{$teacher["initial"]}}
-                            </td>
-
+                            </tr>
                         @endforeach
-                    </tr>
-                @endforeach
-            @endforeach
-        </tbody>
-    </table>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
-</body>
-
-</html>
---
+++ resources/views/_sidebar.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Routine Creation</title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="asset/css/app.css">
+</head>
+
+<body class="bg-gray-200 align-baseline">
+    
+    <div class="sidebar-navigation hidde-sm hidden-xs">
+        <div class="logo">
+            <a href="/">Routine<em>Creation</em></a>
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/routine">
+                        <span class="rect"></span>
+                        <span class="circle"></span>
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a href="/routine" onclick="day()">
+                        <span class="rect"></span>
+                        <span class="circle"></span>
+                        Day
+                    </a>
+                </li>
+                <li>
+                    <a href="#education">
+                        <span class="rect"></span>
+                        <span class="circle"></span>
+                        Slot
+                    </a>
+                </li>
+                <li>
+                    <a href="#cirtification">
+                        <span class="rect"></span>
+                        <span class="circle"></span>
+                        Room
+                    </a>
+                </li>
+                <li>
+                    <a href="#abilities">
+                        <span class="rect"></span>
+                        <span class="circle"></span>
+                        Teacher
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    
+</body>
+</html>
