bdd44810a1402562ddd7260445fac702459fb3c4
Biddut Bhowmik <54990169+bhowmik23@users.noreply.github.com>
2020-10-07 16:26:13 +0600

    room slot view updated

--- resources/views/_room.blade.php
+++ resources/views/_room.blade.php
@@ -26,6 +26,18 @@
                 @enderror
             </div>
             <div class="mb-6">
+                <button type="submit" class="bg-blue-400 text-white rounded-full py-2 px-4 hover:bg-blue-500">
+                    Submit
+                </button>
+            </div>
+        </form>
+    </div>
+</div>  
+<div class="mx-auto my-12 bg-white p-10 rounded-md">
+    <div class="container">  
+        <form action="{{route('dayTimeStore')}}" method="post">
+            @csrf        
+            <div class="mb-6">
                 <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="free_days">Free Days</label>
                 <select class="border border-gray-400 w-full" name="free_days[]" id="free_days" multiple required>
                     <option class="py-2 px-4 border-b border-b-gray-300" value="Sataurday">Sataurday</option>
@@ -41,15 +53,17 @@
                 <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="free_slot">
                     Free Slot
                 </label>
-
-                <input class="border border-gray-400 p-2 w-full" type="text" name="free_slot" id="free_slot"
-                    placeholder="Enter Free SLot" required>
+                <div class="flex">
+                    <input class="border border-gray-400 p-2 w-1/2" type="time" name="start" id="start"placeholder="Enter Time" required>
+                    <label class="block mb-2 font-bold  text-xstext-gray-700 mr-2 ml-2" for="to">To</label>
+                    <input class="border border-gray-400 p-2 w-1/2" type="time" name="end" id="end"placeholder="Enter Time" required>
+                 </div>
                 @error('free_slot')
                 <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
                 @enderror
             </div>
             <div class="mb-6">
-                <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
+                <button type="submit" class="bg-blue-400 text-white rounded-full py-2 px-4 hover:bg-blue-500">
                     Submit
                 </button>
             </div>
--- resources/views/_slot.blade.php
+++ resources/views/_slot.blade.php
@@ -5,25 +5,31 @@
         <form action="{{route('slotStore')}}" method="post">
             @csrf
             <div class="mb-6">
-                <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="slot_type">Slot Type</label>
-                <select class="border border-gray-400 p-2 w-full" name="slot_type" id="slot_type" required>
-                    <option class="py-1 border-b border-b-gray-300" value="Summer">Summer</option>
-                    <option class="py-1 border-b border-b-gray-300" value="Winter">Winter</option>>
+                <label class="block mb-2 font-bold textl-g text-gray-700" for="slot_cat">Select Slot</label>
+                <select class="border border-gray-400 p-2 w-full" name="slot_cat" id="slot_cat" required>
+                    <option class="py-1 border-b border-b-gray-300" value="Slot A">Slot A</option>
+                    <option class="py-1 border-b border-b-gray-300" value="Slot B">Slot B</option>
+                    <option class="py-1 border-b border-b-gray-300" value="Slot C">Slot C</option>
+                    <option class="py-1 border-b border-b-gray-300" value="Slot D">Slot D</option>
+                    <option class="py-1 border-b border-b-gray-300" value="Slot E">Slot E</option>
+                    <option class="py-1 border-b border-b-gray-300" value="Slot F">Slot F</option>
                 </select>
             </div>
             <div class="mb-6">
                 <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="time">
                     Time
                 </label>
-
-                <input class="border border-gray-400 p-2 w-full" type="text" name="time" id="time"
-                    placeholder="Enter Time" required>
+                <div class="flex">
+                    <input class="border border-gray-400 p-2 w-1/2" type="time" name="start" id="start"placeholder="Enter Time" required>
+                    <label class="block mb-2 font-bold  text-xstext-gray-700 mr-2 ml-2" for="to">To</label>
+                    <input class="border border-gray-400 p-2 w-1/2" type="time" name="end" id="end"placeholder="Enter Time" required>
+                 </div>
                 @error('time')
                 <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
                 @enderror
             </div>
             <div class="mb-6">
-                <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
+                <button type="submit" class="bg-blue-400 text-white rounded-full py-2 px-4 hover:bg-blue-500">
                     Submit
                 </button>
             </div>
